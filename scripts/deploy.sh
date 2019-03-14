fd9caab15f4f235899120d721bcc3282999087b5
Ben Fleming <benfleming121@gmail.com>
2019-03-14 17:28:37 +1300

    remove old pm2 instance

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-npm ci
-npm run build
-
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 branch_name=${branch_name##refs/heads/}
+
+pm2 delete "BenFlemingIO:${branch_name}"
+
+npm ci
+npm run build
 
 pm2 start npm --name "BenFlemingIO:${branch_name}" -- start
