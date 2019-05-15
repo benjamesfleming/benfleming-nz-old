40ab83b76c4612c8ef6d33153e54406b8ea1ac0e
Ben Fleming <benfleming121@gmail.com>
2019-05-16 10:24:51 +1200

    remove unused icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,14 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
-    ]
+  },
+
+  /*
+  ** PWA Manifest Options 
+  */
+  manifest: {
+    name: 'benfleming.io',
+    lang: 'en'
   },
 
   /*
