640ee53ad57f302e04be4f23b428a243c12df42f
Ben Fleming <benfleming121@gmail.com>
2020-02-02 07:55:17 +1300

    move app markup into index layout

---
+++ layouts/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app class="app">
+        <main v-reverse-order>
+
+            <!-- Page Content -->
+            <nuxt/>
+
+        </main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style lang="stylus" scoped>
+// @import '~assets/style/variables.styl';
+</style>
